fa97494ffbb89c76e08ecf7c8436535afd607f45
serielain <Julian13062002@outlook.com>
2024-04-28 17:52:22 +0200

    universal downloader now seems to work for reddit, only 347 then it blocks.

--- src/general_functions.py
+++ src/general_functions.py
@@ -11,6 +11,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import WebDriverException
 import argparse
+from datetime import datetime
 
 class NoNewImageException(Exception):
     pass
@@ -33,7 +34,7 @@ def parse_arguments(default_type, default_url, default_folder_path):
 
     return args
 
-def pinterest_image_grabber(driver, image_urls, website_type, folder_path):
+def universal_image_grabber(driver, image_urls, website_type, folder_path):
 
     scroll_position = 0 # Initial scroll position
     no_new_image_counter = 0 # Counter for the number of retries with no new image
@@ -45,8 +46,8 @@ def pinterest_image_grabber(driver, image_urls, website_type, folder_path):
             # Scroll down the page in steps
             for _ in range(2):  # Adjust this value based on your needs (default:2)
                 scroll_position += 150  # Scroll down 150 default
-                driver.execute_script(f"window.scrollTo(0, {scroll_position});")
-                time.sleep(0.05)  # Adjust this value based on your needs
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(0.5)  # Adjust this value based on your needs
 
             # image urls before adding new ones
             image_counter_prev = len(image_urls)
@@ -80,7 +81,7 @@ def pinterest_image_grabber(driver, image_urls, website_type, folder_path):
         print(f"No new images were found after {no_new_image_counter} retries. Starting to process the image URLs...")
         driver.quit()
     # Create or open a .txt file to save the modified image URLs
-    with open("changed_list.txt", "w") as file:
+    with open("link_list.txt", "w") as file:
         # Save each unique image URL to the .txt file
         for image_url in image_urls:
             file.write(image_url + "\n")
@@ -88,16 +89,18 @@ def pinterest_image_grabber(driver, image_urls, website_type, folder_path):
     print("All image URLs saved successfully!")
 
     # Count the number of saved URLs
-    with open("changed_list.txt", "r") as file:
+    with open("link_list.txt", "r") as file:
         url_count = sum(1 for line in file)
     print(f"Number of saved URLs: {url_count}")
 
     # Read the modified image URLs from the .txt file and download each image
     image_counter = 0
-    with open("changed_list.txt", "r") as file:
+    with open("link_list.txt", "r") as file:
         for line in file:
             image_url = line.strip()
-            image_name = image_url.split("/")[-1]
+            # Get the current date and time in the format dd_mm_yyyy_hh_min_sec
+            current_time = datetime.now().strftime('%d_%m_%Y_%H_%M_%S')
+            image_name = f"image_{current_time}_{image_counter}.jpg"
             save_path = os.path.join(folder_path, image_name)
 
             # Send a GET request to download the image
@@ -109,48 +112,12 @@ def pinterest_image_grabber(driver, image_urls, website_type, folder_path):
             image_counter += 1
             print(f"Number {image_counter} / {len(image_urls)} downloaded: {image_name}")
 
-        
+    # def download_image(image_url, save_path):
+    #     # Send a GET request to download the image
+    #     image_response = requests.get(image_url)
 
-    def check_and_redownload_images_pinterest():
-        # Read the modified image URLs from the .txt file
-        with open("changed_list.txt", "r") as file:
-            for line in file:
-                image_url = line.strip()
-                image_name = image_url.split("/")[-1]
-                save_path = os.path.join(folder_path, image_name)
+    #     # Save the image to the specified directory
+    #     with open(save_path, "wb") as image_file:
+    #         image_file.write(image_response.content)
 
-                # Check if the image file exists and is valid
-                if os.path.isfile(save_path):
-                    try:
-                        # Open the image file
-                        img = Image.open(save_path)
-                        img.verify()
-                    except (IOError, SyntaxError) as e:
-                        print(f"Invalid image: {image_name}")
-                        # If the image is invalid, re-download it with "564x" instead of "originals"
-                        redownload_url = image_url.replace("originals", "564x")
-                        redownload_image(redownload_url, save_path)
-                else:
-                    print(f"Image not found: {image_name}")
-                    # If the image file does not exist, download it
-                    download_image(image_url, save_path)
-
-    def download_image(image_url, save_path):
-        # Send a GET request to download the image
-        image_response = requests.get(image_url)
-
-        # Save the image to the specified directory
-        with open(save_path, "wb") as image_file:
-            image_file.write(image_response.content)
-
-        print(f"Downloaded: {os.path.basename(save_path)}")
-
-    def redownload_image(image_url, save_path):
-        # Delete the invalid image file
-        os.remove(save_path)
-
-        # Re-download the image
-        download_image(image_url, save_path)
-
-    # Call the function to check and re-download images
-    check_and_redownload_images_pinterest()
+    #     print(f"Downloaded: {os.path.basename(save_path)}")
